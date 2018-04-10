64e45aa54392e3e8ba43f935822f9520481866c6
embedded-iot <nguyenquan5895@gmail.com>
2018-04-10 23:48:53 +0700

    [10/4] Add function printText Matrix

--- NodeESP8266/NodeESP8266.ino
+++ NodeESP8266/NodeESP8266.ino
@@ -187,7 +187,8 @@ void setup()
   mx.setShiftDataOutCallback(scrollDataSink);
 
   scrollDelay = SCROLL_DELAY;
-  PrintMatrix("START!", 0);
+  // PrintMatrix("START!", 0);
+  printText(0, MAX_DEVICES-1, "START!");
   GPIO();
   if (digitalRead(SERVER_PIN) == LOW) {
     isServer = true;
@@ -251,7 +252,8 @@ void setup()
     SendUdp(broadCast.toString(), udpPort,"#S#" + apSSID + "##START#E#");
   }
   show("End Setup()");
-  PrintMatrix("END!     ", 0);
+  // PrintMatrix("END!     ", 0);
+  printText(0, MAX_DEVICES-1, "End!");
   delay(1000);
 }
 
@@ -375,6 +377,9 @@ void loop()
   }
   
   if (resultRF.length() > 0) {
+    // PrintMatrix(getData(resultRF), 0);
+    PrintMatrix("    ", 0);
+    printText(0, MAX_DEVICES-1, string2char(getData(resultRF)));
      if (!client.connected()) {
        client.flush();
        client.stop();
@@ -427,6 +432,11 @@ void show(String s)
   #endif
 }
 
+char* string2char(String command){
+  char *szBuffer = new char[command.length()+1];
+     strcpy(szBuffer,command.c_str( ));
+  return szBuffer; 
+}
 
 void blinkLed(int numberRepeat, long tdelay) {
   int i=0;
@@ -1311,5 +1321,68 @@ void TurnOnScroll() {
 void TurnOffScroll() {
   flagScroll = false;
 }
+
+void printText(uint8_t modStart, uint8_t modEnd, char *pMsg)
+// Print the text string to the LED matrix modules specified.
+// Message area is padded with blank columns after printing.
+{
+  uint8_t   state = 0;
+  uint8_t	  curLen;
+  uint16_t  showLen;
+  uint8_t	  cBuf[8];
+  int16_t   col = ((modEnd + 1) * COL_SIZE) - 1;
+
+  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
+
+  do     // finite state machine to print the characters in the space available
+  {
+    switch(state)
+    {
+      case 0:	// Load the next character from the font table
+        // if we reached end of message, reset the message pointer
+        if (*pMsg == '\0')
+        {
+          showLen = col - (modEnd * COL_SIZE);  // padding characters
+          state = 2;
+          break;
+        }
+
+        // retrieve the next character form the font file
+        showLen = mx.getChar(*pMsg++, sizeof(cBuf)/sizeof(cBuf[0]), cBuf);
+        curLen = 0;
+        state++;
+        // !! deliberately fall through to next state to start displaying
+
+      case 1:	// display the next part of the character
+        mx.setColumn(col--, cBuf[curLen++]);
+
+        // done with font character, now display the space between chars
+        if (curLen == showLen)
+        {
+          showLen = CHAR_SPACING;
+          state = 2;
+        }
+        break;
+
+      case 2: // initialize state for displaying empty columns
+        curLen = 0;
+        state++;
+        // fall through
+
+      case 3:	// display inter-character spacing or end of message padding (blank columns)
+        mx.setColumn(col--, 0);
+        curLen++;
+        if (curLen == showLen)
+          state = 0;
+        break;
+
+      default:
+        col = -1;   // this definitely ends the do loop
+    }
+  } while (col >= (modStart * COL_SIZE));
+
+  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
+}
+
 
 
