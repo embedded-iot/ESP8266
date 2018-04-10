c684758e95ca50e1e8acb267078b498b43c3da50
embedded-iot <nguyenquan5895@gmail.com>
2018-04-10 22:48:46 +0700

    [10/4] Added Matrix led 8x32 7219

--- NodeESP8266/NodeESP8266.ino
+++ NodeESP8266/NodeESP8266.ino
@@ -11,7 +11,10 @@
  * D2 12 GPIO12
  * D3 5  GPIO5
  * 
- * 
+ * Matrix 4x32 7219
+ * CLK_PIN		14   GPIO14 or SCK
+ * DATA_PIN	  13   GPIO13 or MOSI
+ * CS_PIN		  0    GPIO0      // or SS
  **********************************************************************************/
 
 #include <ESP8266WiFi.h>
@@ -20,6 +23,35 @@
 #include <WiFiServer.h>
 #include <EEPROM.h>
 #include <WiFiUdp.h>
+
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+#define	MAX_DEVICES	4
+
+#define	CLK_PIN		14//D5 // or SCK
+#define	DATA_PIN	13//D7 // or MOSI
+#define	CS_PIN		0//D8 // or SS
+
+// SPI hardware interface
+MD_MAX72XX mx = MD_MAX72XX(CS_PIN, MAX_DEVICES);
+// Arbitrary pins
+//MD_MAX72XX mx = MD_MAX72XX(DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+#define SCROLL_DELAY	75	// in milliseconds
+
+#define	CHAR_SPACING	1	// pixels between characters
+
+// Global message buffers shared by Serial and Scrolling functions
+#define	BUF_SIZE	75
+char curMessage[BUF_SIZE];
+char newMessage[BUF_SIZE];
+bool newMessageAvailable = false;
+uint16_t	scrollDelay;	// in milliseconds
+bool flagScroll = false;
 
 ESP8266WebServer server(80);
 
@@ -148,6 +180,14 @@ void setup()
   isLogin = false;
   //WiFi.disconnect();
   EEPROM.begin(512);
+
+  // Display initialisation
+  mx.begin();
+  mx.setShiftDataInCallback(scrollDataSource);
+  mx.setShiftDataOutCallback(scrollDataSink);
+
+  scrollDelay = SCROLL_DELAY;
+  PrintMatrix("START!", 0);
   GPIO();
   if (digitalRead(SERVER_PIN) == LOW) {
     isServer = true;
@@ -171,7 +211,7 @@ void setup()
   //AccessPoint();
   //delay(1000);
   ConnectWifi(timeStation); 
-  
+
   Serial.println("Begin TCP Server");
   tcpServer.begin(); // Start the TCP server port 333
   //delay(1000);
@@ -211,6 +251,7 @@ void setup()
     SendUdp(broadCast.toString(), udpPort,"#S#" + apSSID + "##START#E#");
   }
   show("End Setup()");
+  PrintMatrix("END!     ", 0);
   delay(1000);
 }
 
@@ -231,6 +272,8 @@ long timeoutNoticeNotConnect = 1 * 60 * 1000;
 void loop()
 {
   server.handleClient();
+  scrollText();
+  
   if (millis() - t > timeLogout) {
     isLogin = false;
     t = millis();
@@ -1165,6 +1208,108 @@ void handleNotFound(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
+}
+void scrollDataSink(uint8_t dev, MD_MAX72XX::transformType_t t, uint8_t col)
+// Callback function for data that is being scrolled off the display
+{
+  #if PRINT_CALLBACK
+  Serial.print("\n cb ");
+  Serial.print(dev);
+  Serial.print(' ');
+  Serial.print(t);
+  Serial.print(' ');
+  Serial.println(col);
+  #endif
+}
+
+uint8_t scrollDataSource(uint8_t dev, MD_MAX72XX::transformType_t t)
+// Callback function for data that is required for scrolling into the display
+{
+  static char		*p = curMessage;
+  static uint8_t	state = 0;
+  static uint8_t	curLen, showLen;
+  static uint8_t	cBuf[8];
+  uint8_t colData;
+
+  // finite state machine to control what we do on the callback
+  switch(state)
+  {
+    case 0:	// Load the next character from the font table
+      showLen = mx.getChar(*p++, sizeof(cBuf)/sizeof(cBuf[0]), cBuf);
+      curLen = 0;
+      state++;
+
+      // if we reached end of message, reset the message pointer
+      if (*p == '\0')
+      {
+        p = curMessage;			// reset the pointer to start of message
+        if (newMessageAvailable)	// there is a new message waiting
+        {
+          strcpy(curMessage, newMessage);	// copy it in
+          newMessageAvailable = false;
+        }
+      }
+      // !! deliberately fall through to next state to start displaying
+
+    case 1:	// display the next part of the character
+      colData = cBuf[curLen++];
+      if (curLen == showLen)
+      {
+        showLen = CHAR_SPACING;
+        curLen = 0;
+        state = 2;
+      }
+      break;
+
+    case 2:	// display inter-character spacing (blank column)
+      colData = 0;
+      curLen++;
+      if (curLen == showLen)
+        state = 0;
+      break;
+
+    default:
+      state = 0;
+  }
+
+  return(colData);
+}
+// Handle scroll Matrix
+void scrollText(void)
+{
+  static uint32_t	prevTime = 0;
+
+  // Is it time to scroll the text?
+  if (flagScroll && millis()-prevTime >= scrollDelay)
+  {
+    mx.transform(MD_MAX72XX::TSL);	// scroll along - the callback will load all the data
+    prevTime = millis();			// starting point for next time
+  }
+}
+
+// Set possition column
+void setColumn(int pos) {
+  pos = MAX_DEVICES * 8 - pos;
+  while (--pos >= 0) {
+    mx.transform(MD_MAX72XX::TSL);
+    delay(10);
+  }
+}
+
+// Display String Matrix
+void PrintMatrix(String s, int pos) {
+  strcpy(curMessage, s.c_str());
+  newMessage[0] = '\0';
+  setColumn(pos);
+}
+
+// Turn on scroll Matrix
+void TurnOnScroll() {
+  flagScroll = true;
+}
+// Turn Off Scroll Matrix
+void TurnOffScroll() {
+  flagScroll = false;
 }
 
 
